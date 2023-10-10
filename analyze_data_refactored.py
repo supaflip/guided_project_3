29ab13e44447c541f1bdff816d80c39d48b6cfa6
My Name <me@example.com>
2023-10-10 16:16:19 -0400

    Finished flask server, began refactoring

---
+++ analyze_data_refactored.py
@@ -0,0 +1,104 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns 
+import pickle
+import matplotlib.pyplot as plt 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
+
+#GLOBALS ####################
+ENABLE_PRINT = False
+SHOW_GRAPHS = True
+DATAFILE = "./data/troop_movements.csv"
+CLASSIFIER = DecisionTreeClassifier
+sns.set()
+
+#FUNCTIONS ####################
+def examine_df(df):
+    print(df.describe())
+    print(df[:5])
+    print(df.dtypes)
+
+def apply_to_cols(fn, *cols):
+    for c in cols:
+        fn(c)
+
+# Create grouped data
+def get_counts(df, col, verbose=False):
+    count = pd.DataFrame(df[col].value_counts())
+    if verbose and ENABLE_PRINT:
+        print(count)
+        print("\n")
+    return count
+
+#Create a bar plot showing value distribution
+def graph_counts(df, col):
+    count = get_counts(df, col, False)
+    sns.barplot(count.T)
+    if SHOW_GRAPHS:
+        plt.show()
+
+def make_model(model_class, X_train, y_train, seed=True, filename="model.pkl"):
+    if seed:
+        model = model_class(random_state=42)
+    else:
+        model = model_class()
+
+    model.fit(X_train, y_train)
+    pickle.dump(model, open(filename, "wb"))
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+    if SHOW_GRAPHS: plt.show()
+
+def graph_weights(model, verbose=False):
+    # Get feature importances
+    importances = model.feature_importances_
+
+    # Create a DataFrame to hold the feature importances
+    feature_importances = pd.DataFrame({'Feature': X.columns, 'Importance': importances})
+    if verbose and ENABLE_PRINT:
+        print (feature_importances)
+
+    # Sorting by highest to lowest Importance
+    sorted_importances = feature_importances.sort_values(by = "Importance", ascending = False)
+
+    # Create a bar plot that shows feature importance
+    if SHOW_GRAPHS:
+        plt.figure(figsize=(8, 6))
+        plt.bar(sorted_importances["Feature"])
+
+#################################
+
+def main(filename, classifier):
+    df = pd.read_csv(filename)
+
+    apply_to_cols(lambda c: get_counts(df, c, True), "empire_or_resistance", "homeworld", "unit_type")
+
+    # Engineer new feature called is_resistance with a True or False value
+    df["is_resistance"] = (df["empire_or_resistance"] == "resistance")
+    if ENABLE_PRINT: print(df["is_resistance"])
+
+    apply_to_cols(lambda c: graph_counts(df, c), "empire_or_resistance")
+
+    # Create prediction model that predicts if character is joining empire or resistance based on homeworld and unit_type
+    X = df[["homeworld", "unit_type"]]
+    y = df["is_resistance"] # boolean in numerical represenation (0, 1)
+
+    X = pd.get_dummies(X)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    if ENABLE_PRINT: print(X_train.shape, y_train.shape)
+
+    model = make_model(classifier, X_train, y_train, filename=f'{filename.strip(".csv")}_model.pkl')
+
+    evaluate_model(model)
+    graph_weights(model)
+
+    return model
+
+main(DATAFILE, CLASSIFIER)
--- app.py
+++ app.py
@@ -2,16 +2,23 @@ from flask import Flask, request, jsonify
 import pickle
 import requests
 import pandas as pd
+import numpy as np
+
+# run: flask run
 
 app = Flask(__name__)
 
-@app.route('/<model_name>/predict')
+@app.route('/<model_name>/predict', methods=['GET', 'POST'])
 def predict(model_name):
-    obs = request.get_json()
-    x = pd.get_dummies(pd.DataFrame(obs))
+    obs = request.args.to_dict()
+    #print(obs)
+    input_features = pd.get_dummies(pd.DataFrame([obs]))
     model = pickle.load(open(f'{model_name}.pkl', 'rb'))
-    pred = model.predict(x)
-    return jsonify(pred)
+    features, x = np.array([(f, int(f in input_features)) for f in model.feature_names_in_]).transpose()
+    print(x)
+    pred = model.predict(x.reshape(1, -1))[0]
+    print(pred)
+    return str(pred)
 
 if __name__ == 'main':
     app.run(port=5000, debug=True)
