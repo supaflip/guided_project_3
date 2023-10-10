fb98f1c6f4e5458c3be1dc979429d0b2c38b5272
Post Man <post@man.com>
2023-10-10 11:32:49 -0400

    Bar Plot Showing Distribution Added

--- analyze_data.py
+++ analyze_data.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
 
 df = pd.read_csv("./data/troop_movements.csv")
+sns.set()
 
 # print(df.describe())
 # print(df[:5])
@@ -19,8 +23,19 @@ def get_counts(df, col):
 cols = ["empire_or_resistance", "homeworld", "unit_type"]
 
 for c in cols:
-    get_counts(df,c)
+    get_counts(df, c)
 
 # Engineer new feature called is_resistance with a True or False value
 df["is_resistance"] = (df["empire_or_resistance"] == "resistance")
 print(df["is_resistance"])
+
+#Create a bar plot showing Empire vs Resistance distribution
+def graph_counts(df, col):
+    count = get_counts(df, col)
+    sns.barplot(count.T)
+    plt.show()
+
+cols2 = ["empire_or_resistance"]
+
+for c in cols2:
+    graph_counts(df, c)
